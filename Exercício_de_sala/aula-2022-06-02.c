c6c6398309a6308cf91b260224ae0c2bdb0d213a
AnthonyBarbiere <106778917+AnthonyBarbiere@users.noreply.github.com>
2022-06-03 21:49:31 -0300

    Update aula-2022-06-02.c

--- Exercício_de_sala/aula-2022-06-02.c
+++ Exercício_de_sala/aula-2022-06-02.c
@@ -42,3 +42,39 @@ scanf("%f",&friE);
 printf("Digite os valores de saída friS")
 scanf("%f",&friS);
 }
+
+ 
+ 
+ #include <stdio.h>
+int main() {
+
+//variáveis
+float imc, massa, altura;
+
+//início
+printf("informe o seu peso:");
+scanf("%f", &massa);
+
+printf("informe sua altura");
+scanf("%f",&altura);
+
+imc = massa/(altura*altura);
+
+if(imc < 18.5)
+   printf("magro\n");
+
+else if(imc < 25)
+   printf("normal\n");
+ 
+else if(imc < 30)
+   printf("sobrepeso\n");
+   
+else printf("obeso\n");      
+  
+
+
+
+
+
+
+}
