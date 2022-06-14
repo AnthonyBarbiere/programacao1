e1a1142af26862eeb2a111d18dac1b6e2315541f
AnthonyBarbiere <106778917+AnthonyBarbiere@users.noreply.github.com>
2022-06-14 15:51:51 -0300

    Add files via upload

---
+++ Exercício_de_sala/exerc_cap2_5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+
+    
+int k;
+
+printf("Informe o número:\n");
+scanf("%d", &k);
+
+if ((k % 3) == 0 && (k % 7) == 0)
+    printf("É divisível por 3 e 7.");
+
+else 
+    printf("Não é divisível por 3 ou 7");
+    
+    
+    
+    
+ }
