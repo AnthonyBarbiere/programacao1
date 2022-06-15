ac42d6a588b664c71775891fa246d24ae28a521e
AnthonyBarbiere <106778917+AnthonyBarbiere@users.noreply.github.com>
2022-06-15 18:10:23 -0300

    Update exerc_cap2_9.c

--- Exercício_de_sala/exerc_cap2_9.c
+++ Exercício_de_sala/exerc_cap2_9.c
@@ -1,1 +1,18 @@
+# inclui  < stdio.h >
+int  principal ()
+{
+  int nasc, anoAtual, idade;
+printf ( " Informe seu ano de nascimento: \n " );
+scanf ( " %d " , &nasc);
+printf ( " Informe o ano atual: \n " );
+scanf ( " %d " , &anoAtual);
 
+  if (nasc > 0 && nasc <= anoAtual) {
+idade = anoAtual - nasc;
+  printf ( " Sua idade é: %d " , idade);
+  }
+
+  senão
+printf ( " O ano inserido não é válido " );
+
+} 	  
