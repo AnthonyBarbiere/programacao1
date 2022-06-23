43d6041f90ea9f3ca2dee25ad20b4062ccbe830b
AnthonyBarbiere <106778917+AnthonyBarbiere@users.noreply.github.com>
2022-06-23 19:47:47 -0300

    Update aula-22-06-23_exerc_3.c

--- Exercício_de_sala/aula-22-06-23_exerc_3.c
+++ Exercício_de_sala/aula-22-06-23_exerc_3.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+int main()
+{
 
+char opcao;
+do {
+printf("Escolha uma opção:\n");
+printf(" a) 10\n");
+printf(" b) 20\n");
+printf(" c) 30\n");
+scanf("%c", &opcao);
+
+
+} while( opcao != 'a' &&
+         opcao != 'b' &&
+         opcao != 'c' );
+         
+         printf(" Sua opção:%c\n", opcao);
+         
+
+
+
+
+
+
+
+
+}
